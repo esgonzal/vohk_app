57fecb097653c733bfa38a75d4d11522f86326f6
esgonzal <esteban.gonzalez.m@mail.pucv.cl>
2026-08-11 12:14:42 -0400

    Add Firebase config for iOS

--- vohk_app/android/app/build.gradle.kts
+++ vohk_app/android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 
 android {
     namespace = "cl.vohk.vohk_app"
-    //compileSdk = flutter.compileSdkVersion
     compileSdk = 36
     ndkVersion = "27.0.12077973"
 
@@ -25,7 +24,7 @@ android {
     defaultConfig {
         applicationId = "cl.vohk.vohk_app"
         minSdk = 26
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
