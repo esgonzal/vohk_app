5f25cf4642b09c2313eff9f8f79efae3cfae89de
Vohk <vohk@iMac-de-Vohk.local>
2026-08-14 15:17:14 -0400

    Preserver working iOS Twilio configuration

--- vohk_app/ios/Runner/AppDelegate.swift
+++ vohk_app/ios/Runner/AppDelegate.swift
@@ -2,12 +2,33 @@ import Flutter
 import UIKit
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  private var incomingCallChannel: FlutterMethodChannel?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+
+    let channel = FlutterMethodChannel(
+      name: "cl.vohk.vohk_app/incoming_call",
+      binaryMessenger: engineBridge.applicationRegistrar.messenger()
+    )
+
+    channel.setMethodCallHandler { call, result in
+      switch call.method {
+      case "consumePendingIncomingCall":
+        result(nil)
+      default:
+        result(FlutterMethodNotImplemented)
+      }
+    }
+
+    incomingCallChannel = channel
   }
 }
