f49c88a6ec657936a75c51091ffa2035fe1e2e77
Vohk <vohk@iMac-de-Vohk.local>
2026-09-21 15:37:30 -0300

    Configure Flutter for Xcode Cloud

---
+++ vohk_app/ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+cd "$CI_PRIMARY_REPOSITORY_PATH"
+
+echo "Installing Flutter..."
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable "$HOME/flutter"
+export PATH="$PATH:$HOME/flutter/bin"
+
+echo "Preparing Flutter for iOS..."
+flutter precache --ios
+
+echo "Getting Flutter dependencies..."
+flutter pub get
+
+echo "Installing CocoaPods..."
+HOMEBREW_NO_AUTO_UPDATE=1
+brew install cocoapods
+
+echo "Installing iOS dependencies..."
+cd ios
+pod install
+
+exit 0
